865968062d4e2e794bab0e8214cd4d8e3793e57d
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-08-20 21:23:14 +0530

    Dark theme added using use state hook and ternerey operetor

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
-import About from './component/About';
+// import About from './component/About';
 import Navbar from './component/Navbar';
-// import TextForm from './component/TextForm';
+import TextForm from './component/TextForm';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = '#13134b';
+
+    }
+    else {
+      setMode('light')
+      document.body.style.background = 'white';
+      
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
       {/* <Navbar/> */}
-      <div className="container">
-        {/* <TextForm heading ="Enter The Text to Analyze" /> */}
-        <About />
-      </div>
+      
+        <TextForm heading ="Enter The Text to Analyze" mode={mode} />
+        {/* <About /> */}
     </>
   );
 }
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -13,19 +13,29 @@ export default function TextForm(props) {
         setText(newText)
     }
     const handleRemoveSpecilChrClick = () => {
-        // console.log("Upper case was clickd")
+    
         
-        let newText = text.replace(/[^a-zA-Z ]/g, "") // it removes numbers also
-        // let newText = text.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "")
-        // let newText = text.replace(/[^a-zA-Z0-9]/g, '');
+        // let newText = text.replace(/[^a-zA-Z ]/g, "") // it removes numbers also
+        // let newText = text.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "") //or write each specil charct
+        let newText = text.replace(/[^a-z A-Z 0-9]/g, " ");
+        // let newText = text.replace(/[^a-z A-Z 0-9]/g, "*");  //it removes special charcter and fill with star
+       
 
         setText(newText)
     }
 
     const handleClearCaseClick = () => {
-        // console.log("Upper case was clickd")
         let newText = ''
         setText(newText)
+    }
+    const handleCopy = () => {
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handlRemExtraSpaces = () => {
+        let newText = text.split(/[  ]+/ )
+        setText(newText.join(" "))
     }
     const handleOnChange = (event) => {
         // console.log("onChange");
@@ -37,26 +47,28 @@ export default function TextForm(props) {
 
     return (
         <>
-            <div className='container'>
+            <div className='container' style= {{color: props.mode=== 'dark' ? 'white' : 'black'}} >
                 <h3>{props.heading} </h3>
-                <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="9"></textarea>
+                <div className="my-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style= {{backgroundColor: props.mode=== 'dark' ? 'gray' : 'white' ,color: props.mode=== 'dark' ? 'white' : 'black' }} id="myBox" rows="9"></textarea>
                 </div>
 
-                <button className="btn btn-info mx-2 " mx-3 onClick={handleUperCaseClick}>Convert Text to UpperCase</button>
-                <button className="btn btn-warning mx-2 " mx-3 onClick={handleLowerCaseClick}>Convert Text to LowerCase</button>
-                <button className="btn btn-danger mx-2 " mx-3 onClick={handleClearCaseClick}>Clear Text</button>
-                <button className="btn btn-success mx-2 " mx-3 onClick={handleRemoveSpecilChrClick }>Remove Speical Charcter</button>
+                <button className="btn btn-info mx-2 " onClick={handleUperCaseClick}>Convert Text to UpperCase</button>
+                <button className="btn btn-warning mx-2 " onClick={handleLowerCaseClick}>Convert Text to LowerCase</button>
+                <button className="btn btn-danger mx-2 " onClick={handleClearCaseClick}>Clear Text</button>
+                <button className="btn btn-success mx-2 " onClick={handleRemoveSpecilChrClick }>Remove Speical Charcter</button>
+                <button className="btn btn-dark mx-2 " onClick={handleCopy}>Copy</button>
+                <button className="btn btn-primary mx-2 " onClick={handlRemExtraSpaces}>Remove Extra spaces</button>
 
             </div>
-            <div className="container">
+            <div className="container" style= {{color: props.mode=== 'dark' ? 'white' : 'black'}}  >
                 <h3>Your Text Summary</h3>
                 <p><b> {text.split(" ").length} </b>Words &<b> {text.length} </b>Character</p>
                 <p><b>{0.008 * text.split(" ").length } </b>--Minutes ro read in slow speed</p>
                 <p><b>{0.0033 * text.split(" ").length}</b> --Minutes ro read in fast speed</p>
                 
                 <h3>Preview</h3>
-                <p>{ text}</p>
+                <p>{ text.length >0 ? text: "Enter something To preview it Here"}</p>
 
             </div>
         </>
